666a853cc3f4d92ad052849bccf73b892febbee9
FeriaStar <clara.rosenkvist@hotmail.com>
2024-10-23 12:33:23 +0200

    Clara - Loading bar trigger update
    Har lavet loading bar fra animation til at skulle være inaktiv som standard og så blive aktiv efter at have trykket på en knap, Trigger. Der er dog nogle errors med koden jeg ikke helt kan se hvorfor

--- Assets/Scripts/LoadingBarEventTrigger.cs
+++ Assets/Scripts/LoadingBarEventTrigger.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DelayedAnimation : MonoBehaviour
+public class LoadingBarEventTrigger : MonoBehaviour
 {
-    private Animator mAnimator;
+    private GameObject screen;
 
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+    
+    void Update()
+    {
+        
+    }
+    public void whenButtonClicked()
+    {
+        if(screen.activeInHierachy == false)
+          screen.setActive(true);
+
+        if(screen.activeInHierachy == true)
+           Destroy (screen, 5.0f);
+    }
+    /*{
         mAnimator = GetComponent<Animator>();
         Debug.Log("Script has started!"); // Check if script starts
+        GameObject.setActive();
 
         if (mAnimator == null)
         {
@@ -28,9 +45,12 @@ public class DelayedAnimation : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.K))
-        {
+        {   
             Debug.Log("K key pressed, playing animation!");
             mAnimator.SetTrigger("TriggerTest");
+            GameObject.setActive();
+            Destroy (gameObject, 5.0f);
         }
-    }
+        
+    }*/
 }
