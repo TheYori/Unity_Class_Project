7f4ea696ce0c240ed4eb2b5249d9e643b7d22a20
Pee-gee-on <39883968+Peegeeon@users.noreply.github.com>
2024-10-28 11:09:25 +0100

    markus test calc
    calcuialtor

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Calculator : MonoBehaviour
+{
+    public TextMeshProUGUI displaytext;
+
+    private string currentInput = "";
+    private double result = 0.0;
+
+    public void OnButtonClick(string buttonValue)
+    {
+        if (buttonValue == "=")
+        {
+            CalculateResult();
+        }
+        else if (buttonValue == "C")
+        {
+            ClearInput();
+        }
+        else
+        {
+            currentInput += buttonValue;
+            UpdateDisplay();
+        }
+    }
+
+    public void CalculateResult()
+    {
+        try
+        {
+            result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentInput, ""));
+            currentInput = result.ToString();
+            UpdateDisplay();
+        }
+        catch (System.Exception)
+        {
+            currentInput = "Error";
+            UpdateDisplay();
+        }
+    }
+
+    private void ClearInput()
+    {
+        currentInput = "";
+        result = 0.0;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        displaytext.text = currentInput;
+    }
+}
+
+
