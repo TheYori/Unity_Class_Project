90764d84cf16b07cbb4348443665951ed5906a0a
danieltovesen <danisop452@gmail.com>
2024-11-01 00:33:06 +0100

    Buttons for Conversations
    buttons to toggle between the conversations with each user

---
+++ Assets/dsadasd.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class dsadasd : MonoBehaviour
+{
+
+    [SerializeField] private string EmilyUI = "Emily1";
+ //
+ public void EmilyButton()
+    {
+        SceneManager.LoadScene(EmilyUI);
+    }
+}
